a94267434befbfe25c84ca71b6392116ca569549
karthikeyan10122k3 <karthikeyan10122k3@gmail.com>
2023-10-14 19:00:01 +0530

    new

--- EndOfFile.java
+++ EndOfFile.java
@@ -3,17 +3,17 @@ package com.kar.practice.exercise;
 import java.util.Scanner;
 
 //To Checks Number of Lines of Inputs
+
 public class EndOfFile {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int lineNumber = 0;
 
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
             lineNumber++;
             System.out.println(lineNumber + " " + line);
         }
 
-        scanner.close();
     }
 }
