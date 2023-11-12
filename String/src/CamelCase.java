4480c04df52e60358cdef29bb32c40cca0be8ee6
karthikeyan10122k3 <karthikeyan10122k3@gmail.com>
2023-11-12 17:22:13 +0530

    commit

---
+++ String/src/CamelCase.java
@@ -0,0 +1,22 @@
+package com.kar.practice.exercise.String.src;
+
+import java.util.Scanner;
+
+public class CamelCase {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter String :");
+        String str = sc.nextLine();
+        System.out.println("Number of Words in the CameCase is : "+countWords(str));
+    }
+    public static int countWords(String str){
+        int count=1;
+        for (int i = 1; i < str.length()-1; i++) {
+            if(Character.isUpperCase(str.charAt(i))){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
